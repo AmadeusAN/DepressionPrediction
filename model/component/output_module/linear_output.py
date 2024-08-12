e9c472c0aadfd3690dbb03bb506d9c69efd7714e
Chenjiahao <a2514642620@gmail.com>
2024-08-12 11:16:48 +0800

    增加输出层，准备进行梯度计算预实验

---
+++ model/component/output_module/linear_output.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import math
+
+
+class LinearOutput(nn.Module):
+    def __init__(
+        self,
+        input_size: int = 3075,
+        layer: int = 6,
+        output_size: int = 1,
+        dropout: float = 0.1,
+    ):
+        """简易的线性输出层，输出 SDS 分数。
+
+        Args:
+            input_size (int, optional): 输入的融入向量. Defaults to 3075.
+            output_size (int, optional): 实值. Defaults to 1.
+        """
+        super().__init__()
+        self.input_size = input_size
+        self.layer = layer
+        self.output_size = output_size
+        self.output_layer = nn.ModuleList()
+
+        cur_input_size = self.input_size
+        if (self.input_size / (math.pow(2, self.layer))) > self.output_size:
+            for i in range(self.layer - 1):
+                output_size = cur_input_size - cur_input_size // 2
+                self.output_layer.append(nn.Linear(cur_input_size, output_size))
+                self.output_layer.append(nn.ReLU())
+                self.output_layer.append(nn.Dropout(dropout))
+                cur_input_size = output_size
+            self.output_layer.append(nn.Linear(cur_input_size, self.output_size))
+
+        else:
+            diff = (self.input_size - self.hidden_size) // self.layer
+            for i in range(self.layer - 1):
+                output_size = cur_input_size - diff
+                self.output_layer.append(nn.Linear(cur_input_size, output_size))
+                self.output_layer.append(nn.ReLU())
+                self.output_layer.append(nn.Dropout(dropout))
+                cur_input_size = output_size
+            self.output_layer.append(nn.Linear(cur_input_size, self.output_size))
+
+    def forward(self, x):
+        for layer in self.output_layer:
+            x = layer(x)
+        return x
+
+
+if __name__ == "__main__":
+    model = LinearOutput()
+    dummy_x = torch.randn(1, 3075)
+    print(model(dummy_x))
+    print(f"shape : {model(dummy_x).shape}")
--- model/model.py
+++ model/model.py
@@ -5,6 +5,7 @@ from component.emotion_path.Wav2vec import Wav2Vec
 from component.text_path.SentenceModel import SentenceModel
 from component.time_frequency_path.TimeFrequencyModel import TFModel
 from component.GFN_module.GFN import GFN
+from component.output_module.linear_output import LinearOutput
 import numpy as np
 import matplotlib.pyplot as plt
 import boto3
@@ -15,35 +16,38 @@ from IPython.display import Audio
 from torchaudio.utils import download_asset
 import torchaudio
 
+
 def preliminary_experiment():
     # load data
     audio_path = r"/public1/cjh/workspace/DepressionPrediction/dataset/EATD-Corpus/train/2/positive.wav"
     text_path = r"/public1/cjh/workspace/DepressionPrediction/dataset/EATD-Corpus/train/2/positive.txt"
-    with open(text_path, 'r') as f:
+    with open(text_path, "r") as f:
         text = f.read()
     tokens = [text]
-    
+
     waveform, sample_rate = torchaudio.load(audio_path)
     if waveform.shape[0] > 1:
         waveform = waveform[0]
-    waveform = torch.unsqueeze(waveform,dim = 0)
+    waveform = torch.unsqueeze(waveform, dim=0)
     waveform.shape
-    
+
     # load model
     wav2vec = Wav2Vec()
     sentence_model = SentenceModel()
     time_frequency_model = TFModel()
     ae = AE()
     gfn = GFN()
-    
+
     emotion_vector = wav2vec(waveform)[0]
-    emotion_vector = torch.concat([emotion_vector,torch.zeros(size = (emotion_vector.shape[0],1))],dim = -1)
-    
+    emotion_vector = torch.concat(
+        [emotion_vector, torch.zeros(size=(emotion_vector.shape[0], 1))], dim=-1
+    )
+
     text_vector = sentence_model.encode(tokens)
     text_vector_enhance = ae(torch.tensor(text_vector))
     tf_vector = time_frequency_model(waveform)
-    final_vector = gfn(text_vector_enhance,emotion_vector,tf_vector)
-    
+    final_vector = gfn(text_vector_enhance, emotion_vector, tf_vector)
+
     print(text)
     print(text_vector.shape)
     print(f"text_vector_enhance: {text_vector_enhance.shape}")
@@ -53,7 +57,7 @@ def preliminary_experiment():
 
 
 class Model(nn.Module):
-    def __init__(self, output_layers:nn.Module = None):
+    def __init__(self, output_layers: nn.Module = None):
         super(Model, self).__init__()
         self.wav2vec = Wav2Vec()
         self.sentence_model = SentenceModel()
@@ -61,38 +65,45 @@ class Model(nn.Module):
         self.ae = AE()
         self.gfn = GFN()
         self.output = output_layers
-        
-    def forward(self, waveform: torch.Tensor = None, text:str = None):
+
+    def forward(self, waveform: torch.Tensor = None, text: str = None):
         """对音频向量进行前向运算
 
         Args:
             x (torch.Tensor): input tensor
         """
         emotion_vector = self.wav2vec(waveform)[0]
-        emotion_vector = torch.concat([emotion_vector,torch.zeros(size = (emotion_vector.shape[0],1))],dim = -1)
+        emotion_vector = torch.concat(
+            [emotion_vector, torch.zeros(size=(emotion_vector.shape[0], 1))], dim=-1
+        )
         text_vector = self.sentence_model.encode(text)
         text_vector_enhance = self.ae(torch.tensor(text_vector))
         tf_vector = self.time_frequency_model(waveform)
-        final_vector = self.gfn(text_vector_enhance,emotion_vector,tf_vector)
-        
-        return final_vector
-        
-        
+        final_vector = self.gfn(text_vector_enhance, emotion_vector, tf_vector)
 
-if __name__ == '__main__':
+        if self.output is not None:
+            final_vector = self.output(final_vector)
+
+        return final_vector
+
+
+if __name__ == "__main__":
     # load data
     audio_path = r"/public1/cjh/workspace/DepressionPrediction/dataset/EATD-Corpus/train/2/positive.wav"
     text_path = r"/public1/cjh/workspace/DepressionPrediction/dataset/EATD-Corpus/train/2/positive.txt"
-    with open(text_path, 'r') as f:
+    with open(text_path, "r") as f:
         text = f.read()
     tokens = [text]
-    
+
     waveform, sample_rate = torchaudio.load(audio_path)
     if waveform.shape[0] > 1:
         waveform = waveform[0]
-    waveform = torch.unsqueeze(waveform,dim = 0)
-    
-    model = Model()
-    final_vector = model(waveform,tokens)
+    waveform = torch.unsqueeze(waveform, dim=0)
+
+    output_layer = LinearOutput()
+    model = Model(output_layers=output_layer)
+    model.train()
+    final_vector = model(waveform, tokens)
     print(f"final_vector: {final_vector.shape}")
-    
+
+    # 梯度计算实验
